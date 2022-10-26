64530171d7b26b38841181a2b9383384051a996e
gaurav459433 <gaurav459433@gmail.com>
2022-10-26 09:27:23 +0530

    Snake and Ladder Simulator

---
+++ SnakeLadderProblem.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.rfp212.assignmentsday04;
+
+import java.util.Random;
+
+public class SnakeLadderProblem {
+
+    static int S(int a, int b) {
+
+        Random random = new Random();
+        while (a != 100) {
+            b++;
+            int SinglePlayerRolls = random.nextInt( bound: 6) +1;
+
+        }
+    }
+}
