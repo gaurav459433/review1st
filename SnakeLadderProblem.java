b6fa35e892f7e9209eb4cf4ba38138ca6da39dc9
gaurav459433 <gaurav459433@gmail.com>
2022-10-27 07:12:09 +0530

    Snake & Ladder Simulator

--- SnakeLadderProblem.java
+++ SnakeLadderProblem.java
@@ -1,7 +1,6 @@
 package com.bridgelabz.rfp212.assignmentsday04;
 
 public class SnakeLadderProblem {
-
     public static void main(String[] args) {
         System.out.println("Snake & Ladder Simulator");
     }
